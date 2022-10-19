4ddac87df60f9db39155d230cc7190cd5ae0af2a
Augusto Castilho <castilho@ice.ufjf.br>
2022-10-19 16:14:03 -0300

    Campos e animações iniciais

---
+++ script.js
@@ -0,0 +1,29 @@
+const prevBtns = document.querySelectorAll(".btn-prev")
+const nextBtns = document.querySelectorAll(".btn-next")
+const formSteps = document.querySelectorAll(".form-step")
+
+let formStepsNum = 0;
+
+nextBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        formStepsNum++;
+        updateFormSteps();
+    });
+});
+
+prevBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        formStepsNum--;
+        updateFormSteps();
+    });
+});
+
+function updateFormSteps(){
+
+    formSteps.forEach(formSteps => {
+        formSteps.classList.contains("form-step-active") &&
+        formSteps.classList.remove("form-step-active")
+    })
+
+    formSteps[formStepsNum].classList.add("form-step-active")
+}
